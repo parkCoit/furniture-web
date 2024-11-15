ee2fd3b884782ebe54c0f34c1858096580f74c36
coitpark <minhobag18@gmail.com>
2024-11-15 13:27:17 +0900

    feat : add api, login, auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import AuthPage from "./pages/AuthPage";
+import AuthPage from "@/pages/AuthPage";
+import LoginPage from "@/pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<AuthPage />} />
+        <Route path="/" element={<div>ss</div>} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </div>
   );
