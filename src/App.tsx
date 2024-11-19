0918ba5d7410ffef9d00af4f256a3eda38dfc372
coitpark <minhobag18@gmail.com>
2024-11-19 18:12:07 +0900

    feat : 메인페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import AuthPage from "@/pages/AuthPage";
 import LoginPage from "@/pages/LoginPage";
+import MainPage from "@/pages/MainPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<div>ss</div>} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
