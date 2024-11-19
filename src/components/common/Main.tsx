8468ba400dcc6cbe26de22f701a46a17508f17da
coitpark <minhobag18@gmail.com>
2024-11-19 18:11:32 +0900

    feat : 메인페이지 추가

---
+++ src/components/common/Main.tsx
@@ -0,0 +1,3 @@
+export function Main() {
+  return <div>메인 페이지</div>;
+}
---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,13 @@
+import { Main } from "@/components/common/Main";
+
+export interface MainPageProps {}
+
+const MainPage = (props: MainPageProps) => {
+  return (
+    <div className="p-4">
+      <Main />
+    </div>
+  );
+};
+
+export default MainPage;
